bc73ef6f2bd787bf71f5a79f95a3e7e480d27ab9
Ekaterina Khaikina <newkatrisha@gmail.com>
2025-03-24 12:00:08 +0200

    fix: network error for android

--- config.ts
+++ config.ts
@@ -1,4 +1,8 @@
-const API_HOST = "http://localhost:8000";
+import { Platform } from "react-native";
+
+// Android emulator needs to use 10.0.2.2 to access the host machine's localhost
+const API_HOST =
+  Platform.OS === "android" ? "http://10.0.2.2:8000" : "http://localhost:8000";
 
 export default {
   API_HOST,
